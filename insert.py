aa49e8abce94c46e630d5d26ffb09e682403d459
rmay <majway27.1@gmail.com>
2017-08-10 14:45:59 +0000

    Edit

--- insert.py
+++ insert.py
@@ -3,18 +3,19 @@ import fileinput
 # Note: Print adds returns so use rstrip
 
 file = "index.html"
-tag = "<script type"
+findtag = "<script type"
+inserttag = '<script type="text/javascript" src="playermarkers/playermarkers.js"></script>'
 found = 0
 
 for line in fileinput.input(file, inplace=True, backup=".bak"):
     if line == "\n" or line == "\r\n":
         print(line).rstrip()
-    elif line.startswith(tag):
+    elif line.startswith(findtag):
         found = 1
         print(line).rstrip()
-    elif not (line.startswith(tag)) and found == 1:
+    elif not (line.startswith(findtag)) and found == 1:
         found = 0
-        print "test"
+        print inserttag
         print(line).rstrip()
     else:
         print(line).rstrip()
